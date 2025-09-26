01f17737ab1adb1159e4262971a5389f53449c4b
chenyangSBS@github.com <chenyangSBS>
2025-09-26 13:36:18 +0800

    add basic data types test

---
+++ src/main/java/edu/sbs/cs/LightYear.java
@@ -0,0 +1,10 @@
+package edu.sbs.cs;
+
+public class LightYear {
+    public static void main(String[] args) {
+       int speed = 300000;
+       long seconds = 365 * 24 * 60 * 60;
+       long distance = seconds * speed;
+       System.out.println("一光年的距离是：" + distance);
+    }
+}
---
+++ src/main/java/edu/sbs/cs/Number.java
@@ -0,0 +1,21 @@
+package edu.sbs.cs;
+
+public class Number {
+
+    public static void main(String[] args) {
+        int b = 200;
+        short decimal = 43;
+        int binary = 0b101;
+        int octal = 052;
+        int hex = 0x2A;
+
+        char charA = 'A';
+        int numberA = charA;
+        System.out.println(charA);
+        System.out.println(numberA);
+
+        String stringTest = "a";
+
+    }
+
+}
